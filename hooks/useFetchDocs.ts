a0d5217518c17e57d66605b5d92750051a8b6558
simei <aq.simei@gmail.com>
2025-03-03 06:05:44 -0300

    fix: adjust fetch docs to run when user is authenticated

--- hooks/useFetchDocs.ts
+++ hooks/useFetchDocs.ts
@@ -1,10 +1,13 @@
 import { fetchDocs } from "@/lib/api/queries/fetchDocs";
+import { useAuth } from "@/providers/Auth/AuthProvider";
 
 import { useQuery } from "@tanstack/react-query";
 export const useFetchDocs = () => {
+  const {isAuthenticated} = useAuth();
   const { data, isSuccess, isLoading } = useQuery({
     queryKey: ["documents"],
     queryFn: () => fetchDocs(),
+    enabled: isAuthenticated
   });
   return {
     documents: data || [],
