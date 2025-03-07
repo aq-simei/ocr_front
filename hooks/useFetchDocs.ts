a78a180548bdca6a11e7811a7528997f0e90d11f
simei <aq.simei@gmail.com>
2025-03-07 08:39:54 -0300

    fix: update fetch docs to not be done without a bearer token

--- hooks/useFetchDocs.ts
+++ hooks/useFetchDocs.ts
@@ -1,17 +1,21 @@
+import api from "@/lib/api";
 import { fetchDocs } from "@/lib/api/queries/fetchDocs";
 import { useAuth } from "@/providers/Auth/AuthProvider";
 
 import { useQuery } from "@tanstack/react-query";
 export const useFetchDocs = () => {
-  const {isAuthenticated} = useAuth();
+  const { isAuthenticated } = useAuth();
   const { data, isSuccess, isLoading } = useQuery({
     queryKey: ["documents"],
     queryFn: () => fetchDocs(),
-    enabled: isAuthenticated
+    enabled:
+      isAuthenticated &&
+      api.defaults.headers.common["Authorization"] !== undefined,
+    retry: 1,
   });
   return {
     documents: data || [],
     isSuccess,
-    isLoading
+    isLoading,
   };
 };
